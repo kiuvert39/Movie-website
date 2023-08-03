8fbd17d789587f0874b86fc10aa76c2724505f9d
kliuvert <galvineegbe@gmail.com>
2023-08-03 11:52:34 +0100

    adding more styling in the search modal

--- src/component/Narvbar.jsx
+++ src/component/Narvbar.jsx
@@ -49,11 +49,10 @@ function MyVerticallyCenteredModal(props){
 
           <Modal.Body>
             <div class="input-group">
-              <input type="search" class="form-control rounded" placeholder="Search"aria-label="Search"aria-describedby="search-addon" />
-              <button type="button" class="btn btn-outline-primary">search</button>
+              <input type="search" class="form-control rounded" placeholder="Search"aria-label="Search"aria-describedby="search-addon" />             
             </div>
           </Modal.Body>          
-            <Button onClick={props.onHide}>Close</Button>          
+                    
       </Modal>
       </>
     )
